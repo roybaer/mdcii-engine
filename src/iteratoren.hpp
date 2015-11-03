cad6ab526c49fa1579c31529af6ba92c693f175d
Benedikt Freisen <b.freisen@gmx.net>
2015-11-03 13:58:20 +0100

    erweitere Nordostiterator zum vollständigen forward_iterator

--- src/iteratoren.hpp
+++ src/iteratoren.hpp
@@ -21,23 +21,33 @@
 
 #include <iterator>
 
-class Nordostiterator : public std::iterator<std::input_iterator_tag, inselfeld_t>
+class Nordostiterator : public std::iterator<std::forward_iterator_tag, inselfeld_t>
 {
-  Welt& welt;
+  Welt *welt;
   int x, y;
+  inselfeld_t temp_feld;  // wird von den Methoden initialisiert, die es benutzen
   
 public:
-  Nordostiterator(Welt& welt, int x, int y) : welt(welt), x(x), y(y) { }
+  Nordostiterator() : welt(nullptr), x(0), y(0) { }
   Nordostiterator(const Nordostiterator& it) : welt(it.welt), x(it.x), y(it.y) { }
+  Nordostiterator(Welt& welt, int x, int y) : welt(&welt), x(x), y(y) { }
+  Nordostiterator& operator=(const Nordostiterator it) { welt = it.welt; x = it.x; y = it.y; }
+  void swap(Nordostiterator& it) { Nordostiterator temp = it; *this = it; it = temp; }
   Nordostiterator& operator++() { ++x; --y; return *this; }
   Nordostiterator operator++(int) { Nordostiterator temp(*this); operator++(); return temp; }
-  bool operator==(const Nordostiterator& r) { return &welt == &r.welt && x == r.x && y == r.y; }
-  bool operator!=(const Nordostiterator& r) { return &welt != &r.welt || x != r.x || y != r.y; }
-  inselfeld_t operator*() { inselfeld_t feld; welt.feld_an_pos(feld, x, y); return feld; }
+  bool operator==(const Nordostiterator& r) { return welt == r.welt && x == r.x && y == r.y; }
+  bool operator!=(const Nordostiterator& r) { return welt != r.welt || x != r.x || y != r.y; }
+  const inselfeld_t& operator*() { welt->feld_an_pos(temp_feld, x, y); return temp_feld; }
+  const inselfeld_t* operator->() { return &**this; }
   
   int gib_x() { return x; }
   int gib_y() { return y; }
 };
+
+void swap(Nordostiterator& it1, Nordostiterator& it2)
+{
+  it1.swap(it2);
+}
 
 class Nordoststreifen
 {
